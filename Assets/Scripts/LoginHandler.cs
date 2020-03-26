fe4a75febeb7e0d211293025c5352e442a28e694
Juan Ramirez <juanmanuelramirez@users.noreply.github.com>
2020-03-26 17:02:28 -0600

    first commit

---
+++ Assets/Scripts/LoginHandler.cs
@@ -0,0 +1,43 @@
+﻿using Firebase.Extensions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginHandler : MonoBehaviour
+{
+
+    public Text emailText;
+    public Text passwordText;
+
+    private string logText = "";
+
+    Firebase.DependencyStatus dependencyStatus = Firebase.DependencyStatus.UnavailableOther;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                SessionHandler.InitializeFirebase(this);
+            }
+            else
+            {
+                Debug.LogError(
+                  "Could not resolve all Firebase dependencies: " + dependencyStatus);
+            }
+        });
+    }
+
+    public void CrearUsuarioPorEmail()
+    {
+        SessionHandler.CreateUserWithEmail(emailText.text, passwordText.text);
+    }
+
+    public void Login()
+    {
+        SessionHandler.LoginByEmail(emailText.text, passwordText.text);
+    }
+}
---
+++ Assets/Scripts/SessionHandler.cs
@@ -0,0 +1,133 @@
+﻿using Firebase;
+using Firebase.Auth;
+using Firebase.Extensions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SessionHandler 
+{
+    public static FirebaseAuth auth;
+    public static FirebaseUser user;
+
+    private static string email = "";
+    private static string password = "";
+
+    public static string uid;
+
+    const int kMaxLogSize = 16382;
+
+    [SerializeField]
+    private static FirebaseUser userLoged = null;
+    private static string logText = "";
+    private static bool fetchingToken = false;
+    public static void InitializeFirebase(object obj)
+    {
+        DebugLog("Setting up Firebase Auth");
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged;
+        auth.IdTokenChanged += IdTokenChanged;
+        
+        AuthStateChanged(obj, null);
+    }
+
+    public static void IdTokenChanged(object sender, System.EventArgs eventArgs)
+    {
+        Firebase.Auth.FirebaseAuth senderAuth = sender as Firebase.Auth.FirebaseAuth;
+        if (senderAuth == auth && senderAuth.CurrentUser != null && !fetchingToken)
+        {
+            senderAuth.CurrentUser.TokenAsync(false).ContinueWithOnMainThread(
+              task => DebugLog(String.Format("Token[0:8] = {0}", task.Result.Substring(0, 8))));
+        }
+    }
+    public static void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        if (auth.CurrentUser != user)
+        {
+            bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+            if (!signedIn && user != null)
+            {
+                DebugLog("Signed out " + user.UserId);
+            }
+            user = auth.CurrentUser;
+            if (signedIn)
+            {
+                DebugLog("Signed in " + user.UserId);
+            }
+        }
+    }
+
+    public static void CreateUserWithEmail(string emailTxt, string passTxt)
+    {
+        email = emailTxt;
+        password = passTxt;
+
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    public static void LoginByEmail(string emailTxt, string passTxt)
+    {
+        string email = emailTxt;
+        string password = passTxt;
+
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            userLoged = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                userLoged.DisplayName, userLoged.UserId);
+
+        });
+    }
+
+    public static void ProfileLoad()
+    {
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        if (user != null)
+        {
+            email = user.Email;
+            System.Uri photo_url = user.PhotoUrl;
+            // The user's Id, unique to the Firebase project.
+            // Do NOT use this value to authenticate with your backend server, if you
+            // have one; use User.TokenAsync() instead.
+            uid = user.UserId;
+        }
+    }
+    public static void DebugLog(string s)
+    {
+        Debug.Log(s);
+        logText += s + "\n";
+
+        while (logText.Length > kMaxLogSize)
+        {
+            int index = logText.IndexOf("\n");
+            logText = logText.Substring(index + 1);
+        }
+    }
+}
